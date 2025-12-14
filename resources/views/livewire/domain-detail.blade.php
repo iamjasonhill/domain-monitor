9ec0cdd6d7e95fae94f95361bafe22a176cdeaa4
Jason Hill <jason@jasonhill.com.au>
2025-12-14 19:36:08 +1000

    Fix remaining regex pattern in DNS records table header

--- resources/views/livewire/domain-detail.blade.php
+++ resources/views/livewire/domain-detail.blade.php
@@ -373,7 +373,7 @@
                                     <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">TTL</th>
                                     <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">Priority</th>
                                     @php
-                                        $isAustralianTld = preg_match('/\.(com|net|org|edu|gov|asn|id)\.au$/', $domain->domain);
+                                        $isAustralianTld = preg_match('/\.(com|net|org|edu|gov|asn|id)\.au$|\.au$/', $domain->domain);
                                     @endphp
                                     @if($isAustralianTld)
                                         <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">Actions</th>
