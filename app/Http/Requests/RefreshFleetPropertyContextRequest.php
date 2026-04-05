cea95b848104b35ea646051a23468c667184f9ce
Jason Hill <jason@jasonhill.com.au>
2026-04-05 13:35:43 +1000

    Add Fleet refresh request validation messages

--- app/Http/Requests/RefreshFleetPropertyContextRequest.php
+++ app/Http/Requests/RefreshFleetPropertyContextRequest.php
@@ -24,4 +24,17 @@ class RefreshFleetPropertyContextRequest extends FormRequest
             'search_console_stale_days' => ['sometimes', 'integer', 'min:1', 'max:30'],
         ];
     }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'force_search_console_api_enrichment.boolean' => 'The force Search Console refresh flag must be true or false.',
+            'search_console_stale_days.integer' => 'The Search Console stale-days override must be a whole number.',
+            'search_console_stale_days.min' => 'The Search Console stale-days override must be at least 1 day.',
+            'search_console_stale_days.max' => 'The Search Console stale-days override may not be greater than 30 days.',
+        ];
+    }
 }
